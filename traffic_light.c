9cc4c16a4b99e4fa20668f5d8a7790e09a6bc146
asarinn <mproemsey@gmail.com>
2018-11-27 20:06:09 -0600

    fixed remaining state logic bugs and timer accuracy

--- traffic_light.c
+++ traffic_light.c
@@ -13,6 +13,7 @@ void set_lights(bit green, bit amber, bit yellow, bit red, bit blue) {
 void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
     TF2 = 0; // clear interrupt flag
     TIMER++; // increment timer
+    FLASH_TIMER++; // increment flash timer
     
     switch (SYSTEM_STATE) {
         case MAIN_GREEN:
@@ -20,6 +21,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         
             if (!SW1) {
                 SYSTEM_STATE = MAIN_YELLOW_SIDE;
+                TIMER = 0;
             }
             else if (!SW2) {
                 SYSTEM_STATE = MAIN_YELLOW_CROSS;
@@ -31,7 +33,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case MAIN_YELLOW_CROSS:
             set_lights(0, 0, 1, 0, 0);
         
-            if (TIMER >= 2000) {
+            if (TIMER >= 1000) {
                 SYSTEM_STATE = MAIN_RED_CROSS;
                 TIMER = 0;
             }
@@ -40,7 +42,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case MAIN_YELLOW_SIDE:
             set_lights(0, 0, 1, 0, 0);
         
-            if (TIMER >= 2000) {
+            if (TIMER >= 1000) {
                 SYSTEM_STATE = MAIN_RED_SIDE;
                 TIMER = 0;
             }
@@ -49,7 +51,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case MAIN_RED_CROSS:
             set_lights(0, 0, 0, 1, 0);
         
-            if (TIMER >= 1000) {
+            if (TIMER >= 500) {
                 SYSTEM_STATE = WALK;
                 TIMER = 0;
             }
@@ -58,7 +60,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case MAIN_RED_SIDE:
             set_lights(0, 0, 0, 1, 0);
         
-            if (TIMER >= 1000) {
+            if (TIMER >= 500) {
                 SYSTEM_STATE = SIDE_GREEN;
                 TIMER = 0;
             }
@@ -67,14 +69,16 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case SIDE_BUTTON_RELEASE:
             set_lights(1, 0, 0, 1, 0);
             
-            if (TIMER >= 5000){
+            if (TIMER >= 2500){
                 SYSTEM_STATE = SIDE_YELLOW;
                 TIMER = 0;
             }
+            
+            break;
         case SIDE_RED:
             set_lights(0, 0, 0, 1, 0);
         
-            if (TIMER >= 1000) {
+            if (TIMER >= 500) {
                 SYSTEM_STATE = MAIN_GREEN;
                 TIMER = 0;
             }
@@ -82,7 +86,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case SIDE_YELLOW:
             set_lights(0, 0, 1, 1, 0);
         
-            if (TIMER >= 2000) {
+            if (TIMER >= 1000) {
                 SYSTEM_STATE = SIDE_RED;
                 TIMER = 0;
             }
@@ -90,7 +94,7 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case SIDE_GREEN:
             set_lights(1, 0, 0, 1, 0);
         
-            if (TIMER >= 30000){
+            if (TIMER >= 15000){
                 SYSTEM_STATE = SIDE_YELLOW;
                 TIMER = 0;
             }
@@ -103,19 +107,20 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case WALK:
             set_lights(0, 0, 0, 1, 1);
         
-            if (TIMER >= 5000) {
+            if (TIMER >= 2500) {
                 SYSTEM_STATE = NO_WALK_ON;
                 TIMER = 0;
+                FLASH_TIMER = 0;
             }
         
             break;
         case NO_WALK_ON:
             set_lights(0, 1, 0, 1, 0);
         
-            if (TIMER >= 5000) {
+            if (TIMER >= 2500) {
                 SYSTEM_STATE = MAIN_GREEN;
             }
-            if (FLASH_TIMER >= 500) {
+            if (FLASH_TIMER >= 250) {
                 SYSTEM_STATE = NO_WALK_OFF;
                 FLASH_TIMER = 0;
             }
@@ -124,10 +129,10 @@ void traffic_light_isr(void) interrupt TIMER_2_OVERFLOW {
         case NO_WALK_OFF:
             set_lights(0, 0, 0, 1, 0);
         
-            if (TIMER >= 5000) {
+            if (TIMER >= 2500) {
                 SYSTEM_STATE = MAIN_GREEN;
             }
-            if (FLASH_TIMER >= 500) {
+            if (FLASH_TIMER >= 250) {
                 SYSTEM_STATE = NO_WALK_ON;
                 FLASH_TIMER = 0;
             }
